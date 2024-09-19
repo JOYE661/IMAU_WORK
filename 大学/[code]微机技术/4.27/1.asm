3d77a34a455875c02a975f29c6d1b8de65cba2c0
JOYE <1752912805@qq.com>
2024-09-19 15:50:46 +0800

    微机技术

---
+++ 大学/[code]微机技术/4.27/1.asm
@@ -0,0 +1,21 @@
+SSTACK SEGMENT STACK
+	   DW 32 DUP(?)			//开辟32位空间
+SSTACK ENDS
+
+CODE SEGMENT
+     ASSUME CS:CODE, SS:SSTACK	//四个段建立连接
+START:PUSH DS
+	  XOR AX, AX		
+	  MOV DS, AX
+	  MOV SI, 3000H		//起始地址
+	  MOV CX, 16		//循环次数16
+AA1:  MOV [SI], AL		//把00H送入　3000H
+	  INC SI			//->3001H
+	  INC AL			//->01H
+	  LOOP AA1			//循环取决cx,cx--
+	  MOV AX,4C00H		//固定循环终止指令1
+	  INT 21H		  	//固定循环终止指令2
+CODE  ENDS
+	  END START
+	  
+	  //D0000起始地址 3000偏移地址
