f9692beb97ecd702bbfe0694c194999700af5cd5
JOYE <1752912805@qq.com>
2025-02-26 19:56:15 +0800

    update

--- .vscodeTest/Test_bysms/Test_bysms/settings.py
+++ .vscodeTest/Test_bysms/Test_bysms/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "bysms",
         "USER": "root",
-        "PASSWORD": "64646464",
+        "PASSWORD":'',
         "HOST": "127.0.0.1",
         "PORT": "3306",
 
