7779f571258738ad4d5c2b0b8a25659e5952bc7b
JOYE <1752912805@qq.com>
2025-02-28 16:39:36 +0800

    update

--- .vscodeTest/Test_bysms/sales/urls.py
+++ .vscodeTest/Test_bysms/sales/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns= [
-    path('orders/',views.listorders),
+    path('deepseek/',views.listorders),
     path('orders1/',views.listorders1),
     path('orders2/',views.listorders2),
     path('orders3/',views.listorders3),
